f4a554bdc632b7b29ea59da3af893d50c329f358
danielschmitz <danieljfa@gmail.com>
2018-11-22 00:56:48 -0200

    alterando para knex

--- libs/db.js
+++ libs/db.js
@@ -1,24 +1,9 @@
 const { Pool } = require('pg')
 require('dotenv').config()
 
-function queryServiceFactory () {
-  const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-  })
-  return (text, params) => pool.query(text, params)
-}
+const db = require('knex')({
+  client: 'pg',
+  connection: process.env.DATABASE_URL
+})
 
-const dbService = app => {
-  app.use((req, res, next) => {
-    const db = {}
-    Object.defineProperties(db, {
-      query: {
-        get: () => queryServiceFactory()
-      }
-    })
-    req.$db = db
-    next()
-  })
-}
-module.exports = dbService
+module.exports = db
--- router/admin.js
+++ router/admin.js
@@ -1,14 +1,22 @@
 const router = require('express').Router()
+const db = require('../libs/db')
 
 router.get('/admin/create-tables', (req, res, next) => {
-  const queries = [
-    /* sql */ `DROP TABLE IF EXISTS users`,
-    /* sql */ `CREATE TABLE users(id SERIAL PRIMARY KEY, firstName VARCHAR(100) not null, lastName VARCHAR(100), email VARCHAR(100))`
-  ]
-
-  req.$db.query(queries.join('; '), null)
-    .then(result => res.send(result))
-    .catch(error => next(error))
+  const users = db.schema.dropTableIfExists('users').then(() => {
+    db.schema.createTable('users', t => {
+      t.increments('id').primary()
+      t.string('first_name', 100)
+      t.string('last_name', 100)
+      t.string('email', 200)
+    })
+  })
+  const tasks = db.schema.dropTableIfExists('tasks').then(() => {
+    db.schema.createTable('tasks', t => {
+      t.increments('id').primary()
+      t.string('name', 100)
+    })
+  })
+  Promise.all([users, tasks]).then(() => res.send('Tabelas Recriadas'))
 })
 
 module.exports = router
