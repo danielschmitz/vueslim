07ea0ce54c8d3657505f932e2edba7fc699c8ae3
Daniel Schmitz <danieljfa@gmail.com>
2018-11-22 14:11:08 -0300

    layout

--- src/App.vue
+++ src/App.vue
@@ -23,29 +23,33 @@
 
     <v-content>
       <v-card>
-        <v-img class="white--text" src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg" aspect-ratio="6">
-          <v-btn flat icon dark @click="drawer=!drawer">
-            <v-icon>menu</v-icon>
-          </v-btn>
+        <v-img class="white--text" src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg" aspect-ratio="9">
 
           <div style="padding-left:15px">
-            <h3 class="headline mb-0">Welcome to Vue!</h3>
+            <h3 class="headline mb-0">
+              <v-btn flat icon dark @click="drawer=!drawer">
+                <v-icon>menu</v-icon>
+              </v-btn>Welcome to Vue!
+            </h3>
             <div>This app uses Vue and Vuetify in the frontend. <br />Express and Knex in the backend</div>
           </div>
 
         </v-img>
-
-        <v-card-text>
-          <router-view />
-        </v-card-text>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn flat color="orange" href="https://github.com/danielschmitz/vueslim" target="_blank">Github project</v-btn>
-          <v-btn flat color="orange" href="http://vueslim.herokuapp.com" target="_blank">Demo app</v-btn>
-        </v-card-actions>
       </v-card>
 
+      <div style="margin:20px">
+        <v-card>
+          <v-card-text>
+            <router-view />
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn flat color="orange" href="https://github.com/danielschmitz/vueslim" target="_blank">Github project</v-btn>
+            <v-btn flat color="orange" href="http://vueslim.herokuapp.com" target="_blank">Demo app</v-btn>
+          </v-card-actions>
+        </v-card>
+      </div>
     </v-content>
   </v-app>
 </template>
