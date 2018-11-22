57903d2fa476a8b8194f30869be6d29c9562599f
Daniel Schmitz <danieljfa@gmail.com>
2018-11-22 09:23:25 -0300

    .env check

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ const router = require('./router')
 const errorHandler = require('./libs/error')
 const start = require('./libs/start')
 
+// check .env file at development
+var fs = require('fs')
+if (process.env.NODE_ENV !== 'production' && !fs.existsSync('./.env')) {
+  throw new Error('.env file missing. Please copy from .env.example')
+}
+
 // express instance
 const app = express()
 
