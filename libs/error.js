23c2662633bd76921a013803f7c70bf746917bb7
danielschmitz <danieljfa@gmail.com>
2018-11-22 21:27:48 -0200

    mensagem de erro padrão no servidor de produção

--- libs/error.js
+++ libs/error.js
@@ -1,6 +1,17 @@
+require('dotenv').config()
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const errorHandler = app => {
   app.use((err, req, res, _next) => {
-    res.status(500).send({ message: err.message, code: err.code, name: err.name })
+    if (isProd) {
+      console.error(err)
+      res
+        .status(500)
+        .send({ message: 'Houve um erro no servidor', code: err.code })
+    } else {
+      res.status(500).send({ message: err.message, code: err.code })
+    }
   })
 }
 module.exports = errorHandler
