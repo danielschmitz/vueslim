58f4e947e310b9969cf086fba45adc8a9853945c
danielschmitz <danieljfa@gmail.com>
2018-11-23 00:10:37 -0200

    User Form Layout

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,139 +1,111 @@
 <script>
-import userService from '@/services/user'
+  import userService from '@/services/user';
 
-export default {
-  name: 'Users',
-  data () {
-    return {
-      users: [],
-      headers: [
-        {
-          text: 'Dessert (100g serving)',
-          align: 'left',
-          sortable: false,
-          value: 'name'
-        },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
-        { text: 'Iron (%)', value: 'iron' }
-      ],
-      desserts: [
-        {
-          value: false,
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: '1%'
-        },
-        {
-          value: false,
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: '1%'
-        },
-        {
-          value: false,
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: '7%'
-        },
-        {
-          value: false,
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: '8%'
-        },
-        {
-          value: false,
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: '16%'
-        },
-        {
-          value: false,
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: '0%'
-        },
-        {
-          value: false,
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: '2%'
-        },
-        {
-          value: false,
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: '45%'
-        },
-        {
-          value: false,
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: '22%'
-        },
-        {
-          value: false,
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: '6%'
-        }
-      ]
+  export default {
+    name: 'Users',
+    data() {
+      return {
+        dialog: false,
+        user: {},
+        users: [],
+        headers: [
+          {
+            text: 'id',
+            align: 'left',
+            sortable: false,
+            value: 'id'
+          },
+          { text: 'First Name', value: 'firstname' },
+          { text: 'Last Name', value: 'lastname' },
+          { text: 'Email', value: 'email' }
+        ]
+      };
+    },
+    mounted() {
+      userService.getAll().then(users => {
+        this.users = users;
+      });
+    },
+    methods: {
+      onNewUserButtonClick() {
+        this.user = {};
+        this.dialog = true;
+      }
     }
-  },
-  mounted () {
-    userService.getAll().then(users => {
-      this.users = users
-    })
-  }
-
-}
+  };
 </script>
 <template>
   <div>
-    <h3>Users</h3>
-    <v-data-table :headers="headers" :items="desserts" class="elevation-0">
-      <template slot="items" slot-scope="props">
-        <td>{{ props.item.name }}</td>
-        <td class="text-xs-right">{{ props.item.calories }}</td>
-        <td class="text-xs-right">{{ props.item.fat }}</td>
-        <td class="text-xs-right">{{ props.item.carbs }}</td>
-        <td class="text-xs-right">{{ props.item.protein }}</td>
-        <td class="text-xs-right">{{ props.item.iron }}</td>
-      </template>
-    </v-data-table>
+    <v-card class="elevation-0">
+      <v-card-title
+        ><h3>Users</h3>
+        <v-spacer></v-spacer>
+        <v-btn @click="onNewUserButtonClick()" color="primary">New User</v-btn>
+      </v-card-title>
+      <v-card-text>
+        <v-data-table
+          :headers="headers"
+          :items="users"
+          class="elevation-0"
+          v-if="users.length>0"
+        >
+          <template slot="items" slot-scope="props">
+            <td>{{ props.item.name }}</td>
+            <td class="text-xs-right">{{ props.item.calories }}</td>
+            <td class="text-xs-right">{{ props.item.fat }}</td>
+            <td class="text-xs-right">{{ props.item.carbs }}</td>
+            <td class="text-xs-right">{{ props.item.protein }}</td>
+            <td class="text-xs-right">{{ props.item.iron }}</td>
+          </template>
+        </v-data-table>
+      </v-card-text>
+    </v-card>
 
+    <v-dialog v-model="dialog" max-width="999">
+      <v-card>
+        <v-card-title class="headline grey lighten-2" primary-title>
+          Usuário
+        </v-card-title>
+
+        <v-card-text>
+          <v-form model="valid">
+            <v-container fluid>
+              <v-layout row wrap>
+                <v-flex xs12 sm6 md4>
+                  <v-text-field
+                    v-model="user.firstname"
+                    label="First Name"
+                    required
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6 md4>
+                  <v-text-field
+                    v-model="user.lastname"
+                    label="Last Name"
+                    required
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm12 md4>
+                  <v-text-field
+                    v-model="user.email"
+                    label="Email"
+                    required
+                  ></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-form>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" flat @click="onSaveUserClick()">
+            Salvar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
