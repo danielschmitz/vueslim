4b41bab201c47c7b2b7b92a57e260fb60b2ca184
danielschmitz <danieljfa@gmail.com>
2018-12-17 22:25:22 -0200

    :bug: Papersize

--- libs/pdf.js
+++ libs/pdf.js
@@ -8,7 +8,7 @@ const pdf = app => {
   router.get('/example', (req, res) => {
     var html = fs.readFileSync('./reports/exemplo.html', 'utf8')
 
-    htmlpdf.create(html, { width: '50mm', height: '30mm' }).toStream((err, stream) => {
+    htmlpdf.create(html, { width: '210mm', height: '297mm' }).toStream((err, stream) => {
       if (err) return res.end(err.stack)
       res.setHeader('Content-type', 'application/pdf')
       stream.pipe(res)
